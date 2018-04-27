b49b37b8b44b17d4ca8a6bd75c6da3b98ceb1637
ggomma <ggomma@gmail.com>
2018-04-27 13:43:28 +0900

    Change sign related function
     1. change signature part to use recovery key
     2. check algorithm and output with go-medibloc

--- src/cryptography/sign.js
+++ src/cryptography/sign.js
@@ -2,23 +2,28 @@ import secp256k1 from 'secp256k1';
 
 
 const recoverPubKeyFromSignature = (msgHash, signature) => {
+  const recovery = parseInt(signature.slice(-2), 16);
   const msgHashBuffer = Buffer.from(msgHash, 'hex');
-  const sigBuffer = Buffer.from(signature, 'hex');
-  return secp256k1.recover(msgHashBuffer, sigBuffer, 1).toString('hex');
+  const sigBuffer = Buffer.from(signature.slice(0, -2), 'hex');
+  return secp256k1.recover(msgHashBuffer, sigBuffer, recovery).toString('hex');
 };
 
 const sign = (privKey, msgHash) => {
   const msgHashBuffer = Buffer.from(msgHash, 'hex');
   const privKeyBuffer = Buffer.from(privKey, 'hex');
   const signature = secp256k1.sign(msgHashBuffer, privKeyBuffer);
-  return signature.signature.toString('hex');
+  const recoveryCode = Buffer.alloc(1);
+  recoveryCode.writeIntBE(signature.recovery, 0, 1);
+
+  return signature.signature.toString('hex') + recoveryCode.toString('hex');
 };
 
 const verifySignature = (pubKey, msgHash, signature) => {
   const msgHashBuffer = Buffer.from(msgHash, 'hex');
   const pubKeyBuffer = Buffer.from(pubKey, 'hex');
-  const signatureBuffer = Buffer.from(signature, 'hex');
+  const signatureBuffer = Buffer.from(signature.slice(0, -2), 'hex');
   const isValid = secp256k1.verify(msgHashBuffer, signatureBuffer, pubKeyBuffer);
+  console.log(isValid);
   return isValid;
 };
 
