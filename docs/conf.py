c774b73ac6488162b38448899ac17db9505e4a03
Jaeyoung Jung <jaeyoung@medibloc.org>
2018-06-05 11:13:22 +0900

    Release v0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'MediBloc'
 # The short X.Y version
 version = u'0.1'
 # The full version, including alpha/beta/rc tags
-release = u'0.1.0'
+release = u'0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
