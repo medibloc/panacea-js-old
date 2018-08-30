2556c81fb104845b450a5b5739fd0a99efa3c130
Jaeyoung Jung <jaeyoung@medibloc.org>
2018-08-30 12:42:29 +0900

    Increase network timeout

--- src/client/constants.js
+++ src/client/constants.js
@@ -3,7 +3,7 @@ export const APPLICATION_JSON = { 'content-type': 'application/json' };
 export const MAX_REQUEST_RETRY_COUNT = 10;
 
 export const STREAM_TIMEOUT = 300000;
-export const NETWORK_TIMEOUT = 1000;
+export const NETWORK_TIMEOUT = 5000;
 
 export const GET = 'get';
 export const POST = 'post';
